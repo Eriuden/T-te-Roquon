e8c88a466b936147e5a4d3aa789496fd495b0143
Eriuden <antoinecarrette@hotmail.fr>
2025-09-01 09:56:29 +0200

    build: article card 01/09/2025

---
+++ Client/src/components/ArticleCard.tsx
@@ -0,0 +1,24 @@
+
+export const ArticleCard = (articleProps: any) => {
+    return (
+      <div key={articleProps._id} className="flex flex-col">
+        <div> { articleProps._id ? (
+          <>
+            <img src={articleProps.picture}/>
+            <h3>{articleProps.name}</h3>
+            <h3>{articleProps.typeArticle}</h3>
+            <h3>{articleProps.groupe}</h3>
+            <h3>{articleProps.price}</h3>
+  
+            <div>
+              <span>{articleProps.likers}</span>
+              <span>{articleProps.dislikers}</span>
+            </div>
+            
+          </>
+        ) : ""
+        }  
+        </div>
+      </div>
+    )
+  }
